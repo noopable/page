07f0931bf4a7cc7bc1633ee5dafb3c5efc6df34e
Tomoaki Kosugi <kipspro@gmail.com>
2013-12-16 16:34:21 +0900

    WIP: tests etc.

---
+++ tests/PageTest/Framework/TestCase.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonModule for the canonical source repository
+ * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace PageTest\Framework;
+/*
+ *
+ *
+ * @copyright Copyright (c) 2013-2013 KipsProduction (http://www.kips.gr.jp)
+ * @license   http://www.kips.gr.jp/newbsd/LICENSE.txt New BSD License
+ */
+use PHPUnit_Framework_TestCase;
+
+class TestCase extends PHPUnit_Framework_TestCase
+{
+
+    public static $locator;
+
+    public static function setLocator($locator)
+    {
+        self::$locator = $locator;
+    }
+
+    public function getLocator()
+    {
+        return self::$locator;
+    }
+}
