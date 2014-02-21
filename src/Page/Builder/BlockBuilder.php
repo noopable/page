7fc8c0aaa20bcfb7a21928b9e2a84f1e353c4204
Tomoaki Kosugi <kipspro@gmail.com>
2014-02-22 00:03:48 +0900

    Add comment about insert second param

--- src/Page/Builder/BlockBuilder.php
+++ src/Page/Builder/BlockBuilder.php
@@ -10,23 +10,23 @@ use Page\Block\BlockInterface;
  */
 class BlockBuilder implements BlockBuilderInterface {
     use ServiceDependency;
-    
+
     protected $block;
-    
+
     public function build(BlockInterface $block){
         $this->block = $block;
         return $block;
     }
-    
+
     //public function getBlock($name, $config = array(), $loadConfig = true)
-    
+
     public function load($name)
     {
         $this->service->loadBlockConfig($name);
     }
-    
+
     /**
-     * 
+     *
      * @param type $name
      * @param type $config
      * @return type
@@ -35,18 +35,18 @@ class BlockBuilder implements BlockBuilderInterface {
     {
         return $this->service->getBlock($name, $config);
     }
-    
+
     public function simpleGet($name)
     {
-        
+
     }
-    
+
     public function setInvokable($name, $class)
     {
         $this->service->getBlocks()->setInvokable($name, $class);
         return $this;
     }
-            
+
     public function block($name, $config = array(), $load = true)
     {
         $config['class'] = 'block';
@@ -56,7 +56,7 @@ class BlockBuilder implements BlockBuilderInterface {
         }
         return $this;
     }
-    
+
     public function insert()
     {
         $args = func_get_args();
@@ -64,25 +64,25 @@ class BlockBuilder implements BlockBuilderInterface {
             if (is_string($arg)) {
                 $name = $arg;
             }
-            
+
             if (is_array($arg)) {
                 $config = $arg;
             }
-            
+
             if (is_bool($arg)) {
                 $load = $arg;
             }
-            
+
             if (is_object($arg)
                 && $arg instanceof \Page\Block\BlockInterface) {
                 $this->block->insertBlock($arg);
             }
         }
-        
+
         if (!isset($config)) {
             $config = array();
         }
-        
+
         if (!isset($name)) {
             if (isset($config['name'])) {
                 $name = $config['name'];
@@ -94,19 +94,23 @@ class BlockBuilder implements BlockBuilderInterface {
                 }
             }
         }
-        
+
         if (!isset($load)) {
             $load = true;
         }
-        
+
+        /**
+         * $config は$block->configure()の引数として渡される。
+         * $config['name'] $config['invokable']などでBlockPluginManagerの動作を変更できる。
+         */
         $block = $this->service->getBlock($name, $config, $load);
         if (isset($this->block)) {
             $this->block->insertBlock($block);
         }
-        
+
         return $this;
     }
-    
+
     public function __invoke() {
         return call_user_func_array(array($this, 'insert'), func_get_args());
     }
